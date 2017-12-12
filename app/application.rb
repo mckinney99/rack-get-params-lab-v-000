2f30e00bc5d7cf79ba00078828cafa4c0c3ee435
Eric McKinney <mckinney99@gmail.com>
2017-12-12 01:45:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,14 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        resp.write "Your cart contains #{@@cart.join("\n")}"
+      end
+      
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
